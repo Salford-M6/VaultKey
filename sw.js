f13b7cc555a4ab6a26b6311dc5d37512a0835ac2
Salford-M6 <pete.birch@comcast.net>
2026-03-31 17:48:02 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+"use strict";
+var CACHE = "vaultkey-v22";
+self.addEventListener("install", function(e) { self.skipWaiting(); });
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    }).then(function(){ self.clients.claim(); })
+  );
+});
+self.addEventListener("fetch", function(e) {
+  e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
+});
